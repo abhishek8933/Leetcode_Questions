2454a1f68840a59612480caefe6e5bfcaacf4c27
Abhishek Maurya <88788373+abhishek8933@users.noreply.github.com>
2022-05-26 14:46:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,10 +4,8 @@ public:
         int ans=0;
         
         while(n>0){
-            if(n&1==1){
-                ans++;
-            }
-            n>>=1;
+           ans++;
+            n=n&(n-1);
         }
         return ans;
     }
