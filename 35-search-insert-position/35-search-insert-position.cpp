db0498e25bb635d6737eab9a2f74a31b217e0736
Abhishek Maurya <88788373+abhishek8933@users.noreply.github.com>
2022-08-21 19:14:20 +0530

    Time: 5 ms (73.08%), Space: 9.8 MB (22.77%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low-(low-high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return high+1;
+    }
+};
