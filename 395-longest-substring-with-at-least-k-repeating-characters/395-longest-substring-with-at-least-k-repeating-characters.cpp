13da2f5f61da11866189cc3ed3e145350ee22e26
Abhishek Maurya <88788373+abhishek8933@users.noreply.github.com>
2022-09-09 19:44:41 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (95.59%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    bool check(int count[],int k){
-        for(int i=0;i<26;i++){
-            if(count[i]!=0&&count[i]<k){
-                return 0;
-            }
+    int solve(string &s,int start,int end,int k){
+        if (end < k) return 0;
+        int countMap[26] = {0};
+        // update the countMap with the count of each character
+        for (int i = start; i < end; i++)
+            countMap[s[i] - 'a']++;
+        for (int mid = start; mid < end; mid++) {
+            if (countMap[s[mid] - 'a'] >= k) continue;
+            int midNext = mid + 1;
+            while (midNext < end && countMap[s[midNext] - 'a'] < k) midNext++;
+            return max(solve(s, start, mid, k),
+                    solve(s, midNext, end, k));
         }
-        return 1;
+        return (end - start);
     }
     int longestSubstring(string s, int k) {
-        int ans=0;
-        for(int i=0;i<s.size();i++){
-            int count[26]={0};
-            for(int j=i;j<s.size();j++){
-                count[s[j]-'a']++;
-                if(count[s[j]-'a']<k){
-                    continue;
-                }
-                if(check(count,k)){
-                    ans=max(ans,j-i+1);
-                }
-            }
-        }
-        return ans;
+        int n=s.size();
+        return solve(s,0,n,k);
     }
 };
