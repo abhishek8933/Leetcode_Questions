608b0f57bd723fffaaaa04ceb798207204d7805e
Abhishek Maurya <88788373+abhishek8933@users.noreply.github.com>
2022-09-24 20:13:10 +0530

    Time: 15 ms (72.38%), Space: 20 MB (49.33%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -12,18 +12,20 @@
 class Solution {
 public:
      vector<vector<int>> final_ans;
-    void solve(TreeNode *root,int target,vector<int>temp){
+    void solve(TreeNode *root,int target,vector<int> &temp){
        if(root==NULL){
            return;
        }
         if(!root->left and !root->right and target == root->val){
             temp.push_back(root->val);
             final_ans.push_back(temp);
+            temp.pop_back();
             return;
         }
         temp.push_back(root->val);
         solve(root->left,target-root->val,temp);
         solve(root->right,target-root->val,temp);
+        temp.pop_back();
         
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
