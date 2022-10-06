4ab09197e5990566afad9a21cf656b564b084ac4
Abhishek Maurya <88788373+abhishek8933@users.noreply.github.com>
2022-10-06 14:28:59 +0530

    Time: 769 ms (18.90%), Space: 128.2 MB (70.01%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -3,16 +3,34 @@ public:
     TimeMap() {
         
     }
-        unordered_map<string,map<int,string>>m;
+    unordered_map<string,vector<pair<string,int>>>map;
+    
+    string search(vector<pair<string,int>> &v,int stamp){
+        int start=0,end=v.size()-1;
+        while(end>=start){
+            int mid=start-(start-end)/2;
+            
+            if(v[mid].second>stamp){
+                end=mid-1;
+            }else if(v[mid].second<stamp){
+                start=mid+1;
+            }else{
+                return v[mid].first;
+            }
+        }
+        
+        return end>=0 ?v[end].first:"";
+    }
+    
     void set(string key, string value, int timestamp) {
-        m[key][timestamp]=value;
+        map[key].push_back({value,timestamp});
     }
     
     string get(string key, int timestamp) {
-        auto it=m[key].upper_bound(timestamp);
-        if(it==m[key].begin())return "";
-        it--;
-        return it->second;
+        if(map.find(key)==map.end()){
+            return "";
+        }
+        return search(map[key],timestamp);
     }
 };
 
