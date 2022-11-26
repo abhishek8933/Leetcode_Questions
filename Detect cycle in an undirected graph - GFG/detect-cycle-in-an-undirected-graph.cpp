243e10f64a6c3665e425d85023d482c0b50816a8
Abhishek Maurya <88788373+abhishek8933@users.noreply.github.com>
2022-11-26 14:59:49 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -4,88 +4,37 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    bool bfs(int sr, vector<int>adj[],int vis[] )
-
-        {
-
-            queue<pair<int,int>>q;
-
-            q.push({sr,-1});
-
-            vis[sr]=1;
-
-            while(!q.empty())
-
-            {
-
-                int node=q.front().first;
-
-                int parent=q.front().second;
-
-                q.pop();
-
-                
-
-                for(auto it:adj[node])
-
-                {
-
-                    if(!vis[it]){
-
-                        vis[it]=1;
-
-                        q.push({it,node});
-
-                    }
-
-                    else if(parent!=it)
-
-                    {
-
-                        return true;
-
-                        
-
-                    }
-
-                }
-
-            }
-
-            
-
-        }
-
   public:
-
     // Function to detect cycle in an undirected graph.
-
-    bool isCycle(int V, vector<int> adj[]) {
-
-        
-
-        int vis[V]={0};
-
-        for(int i=0;i<V;i++){
-
-        if(!vis[i]){
-
-        if(bfs(i,adj,vis)==true)
-
-        {
-
-               return true;     
-
-        }
-
-        }
-
-        }
-
-        return false;
-
-    }
     
+    bool dfs(int node,int parent,vector<int>adj[],vector<int> &visited){
+        visited[node]=1;
+        
+        for(auto it:adj[node]){
+            if(visited[it]==0){
+               if(dfs(it,node,adj,visited)){
+                    return true;
+                }
+            }
+            else if(it!=parent){
+                    return true;
+                }
+        }
+        return false;
+    }    
+    
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<int> visited(V,0);
+        
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(dfs(i,-1,adj,visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 };
 
 //{ Driver Code Starts.
